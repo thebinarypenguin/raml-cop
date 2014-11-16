a9b8e23ad001375a051a7e9d04ff5c955bb27b70
Ethan Zimmerman <thebinarypenguin@gmail.com>
2014-11-16 11:39:15 -0500

    Initial Code Commit

---
+++ src/raml-cop.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+var async     = require('async');
+var commander = require('commander');
+var colors    = require('colors');
+var raml      = require('raml-parser');
+var pkg       = require('../package.json');
+
+// If the --json flag is specified, parsed data is collected and only output if every file is valid
+var collectedData = [];
+var allValid      = true;
+
+// Handle successful parsing of RAML file
+var parseSuccess = function(file, data, callback) {
+  if (commander.json) {
+    if (commander.args.length === 1) {
+      collectedData = data;
+    } else {
+      collectedData.push(data);
+    }
+  } else {
+    console.log('[' + file + '] '+'valid'.green);
+  }
+
+  callback();
+};
+
+// Handle failed parsing of RAML file
+var parseFailure = function(file, err, callback) {
+  allValid = false;
+
+  if (err.problem_mark) {
+    console.error('[' + err.problem_mark.name + ':' + err.problem_mark.line + ':' +
+      err.problem_mark.column + '] ' + err.message.red);
+  } else {
+    console.error('[' + file + '] '+err.message.red);
+  }
+
+  callback();
+};
+
+// Define command line options
+commander
+  .version(pkg.version)
+  .usage('[options] <file ...>')
+  .option('-j, --json', 'output JSON')
+  .parse(process.argv);
+
+// If no files specified, display usage message and exit
+if (!commander.args.length) {
+  commander.help();
+}
+
+// Parse each file in sequence.
+async.eachSeries(commander.args, function(file, callback) {
+  raml.loadFile(file).then(function(data) {
+    parseSuccess(file, data, callback);
+  }, function(err) {
+    parseFailure(file, err, callback);
+  })
+}, function(err) {
+  // err is not used and should always be undefined.
+  // Any parse errors are handled completely by parseFailure()
+
+  // If the --json flag is specified and all files are valid, output the parsed data as JSON
+  if (commander.json && allValid) {
+    console.log(JSON.stringify(collectedData, null, '  '));
+  }
+});
