101511c577800209dbc9b96382e21e22b9ca593e
Ethan Zimmerman <thebinarypenguin@gmail.com>
2016-10-03 15:13:09 -0400

    Drop grunt in favor of npm scripts based workflow

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "rules": {
+    "comma-dangle": [ "error", "always-multiline" ],
+    "indent": [ "error", 2 ],
+    "no-console": "off",
+    "semi": [ "error", "always" ],
+    "strict": [ "error", "safe" ],
+  },
+  "env": {
+    "es6": true,
+    "node": true,
+  },
+  "extends": "eslint:recommended",
+};
